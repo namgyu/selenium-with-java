b0bf301fd3fbefe23f65cac96a62e687037d2e87
국남규 <3gguild@naver.com>
2018-01-19 16:22:28 +0900

    init selenium-with-java

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumTest {
+
+    @Test
+    public void login() {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "webdriver/chrome/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+
+        System.out.println("[Prev] Page title is: " + driver.getTitle());
+
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("webdriver");
+        element.submit();
+
+        // 10초 대기
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("webdriver");
+            }
+        });
+
+        System.out.println("[Next] Page title is: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
