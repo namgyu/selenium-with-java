99a5a36e371a60218fc9a1e2e1e7e7487c1cca11
국남규 <3gguild@naver.com>
2018-01-29 11:09:22 +0900

    chore

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class SeleniumTest {
 
     @Test
-    public void login() {
+    public void searchWebdriver() {
         WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "webdriver/chrome/chromedriver");
         driver = new ChromeDriver();
@@ -21,14 +21,14 @@ public class SeleniumTest {
         element.sendKeys("webdriver");
         element.submit();
 
+        System.out.println("[Next] Page title is: " + driver.getTitle());
+
         // 10초 대기
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
                 return d.getTitle().toLowerCase().startsWith("webdriver");
             }
         });
-
-        System.out.println("[Next] Page title is: " + driver.getTitle());
 
         driver.quit();
     }
